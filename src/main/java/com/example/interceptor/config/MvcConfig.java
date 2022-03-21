fae661e9d4b27b66892d207544c6b7bbd2b2231e
sujeongHeo401 <gotnwjd40@gmail.com>
2022-03-22 01:05:17 +0900

    Initial commit

---
+++ src/main/java/com/example/interceptor/config/MvcConfig.java
@@ -0,0 +1,21 @@
+package com.example.interceptor.config;
+
+import com.example.interceptor.interceptor.AuthInterceptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class MvcConfig implements WebMvcConfigurer {
+//    @Autowired ----> 순환 참졸 받을 수 있음
+    private final AuthInterceptor authInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authInterceptor).addPathPatterns("/api/private/*");
+
+    }
+}
